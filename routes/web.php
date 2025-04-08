12593aac3fde59d45ffaf727d1fa2829114eac6e
Antonio-ckl <162490864+Antonio-ckl@users.noreply.github.com>
2025-04-08 08:16:37 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Livewire\Admin\Dashboard;
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/asdf', Login::class)->name('login');
+Route::get('/', Login::class)->name('login');
 
 Route::get('/admin/dashboard', Dashboard::class)->middleware(['auth','user_type:admin'])->name('admin.dashboard');
